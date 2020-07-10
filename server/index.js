765b969a9a2d11e0fd7d3b469735e2a726d1e961
Phil Nash <philnash@gmail.com>
2020-07-10 12:27:28 +1000

    Split server code from running server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,6 @@
-const { join } = require("path");
-const express = require("express");
-const pino = require("pino")();
-const expressLogger = require("express-pino-logger")({
-  logger: pino,
-});
-const tokensController = require("./controllers/tokensController");
-const app = express();
-
 const config = require("./config");
-
-app.use(expressLogger);
-app.use(express.static(join(__dirname, "..", "client", "dist")));
-
-app.use("/tokens", tokensController);
+const { app, logger } = require("./server");
 
 app.listen(config.port, () => {
-  pino.info(`Listening on localhost:${config.port}`);
+  logger.info(`Listening on localhost:${config.port}`);
 });
