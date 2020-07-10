dbbd4e6a03b4f159d89887f6c58f99034c9fd0d1
Phil Nash <philnash@gmail.com>
2020-07-10 12:24:00 +1000

    Adds tokens controller

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ const pino = require("pino")();
 const expressLogger = require("express-pino-logger")({
   logger: pino,
 });
+const tokensController = require("./controllers/tokensController");
 const app = express();
 
 const config = require("./config");
 
 app.use(expressLogger);
 app.use(express.static(join(__dirname, "..", "client", "dist")));
+
+app.use("/tokens", tokensController);
 
 app.listen(config.port, () => {
   pino.info(`Listening on localhost:${config.port}`);
