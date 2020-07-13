f815d50dca2b8ce73fe55294b330c28c063194e3
Phil Nash <philnash@gmail.com>
2020-07-13 10:17:03 +1000

    Changing some naming

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const { join } = require("path");
 const express = require("express");
-const pino = require("pino")();
+const logger = require("pino")();
 const expressLogger = require("express-pino-logger")({
-  logger: pino,
+  logger,
 });
 const tokensController = require("./controllers/tokensController");
 const app = express();
@@ -12,4 +12,4 @@ app.use(express.static(join(__dirname, "..", "client", "dist")));
 
 app.use("/tokens", tokensController);
 
-module.exports = { app, logger: pino };
+module.exports = { app, logger };
