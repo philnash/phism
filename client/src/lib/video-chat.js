aa494bcae12b1b728f4487a287206289e18d0954
Phil Nash <philnash@gmail.com>
2020-08-31 10:02:21 +1000

    Corrects unsubscribed typo

--- client/src/lib/video-chat.js
+++ client/src/lib/video-chat.js
@@ -18,7 +18,7 @@ export class VideoChat extends EventTarget {
     this.trackPublished = this.trackPublished.bind(this);
     this.trackUnpublished = this.trackUnpublished.bind(this);
     this.trackSubscribed = this.trackSubscribed.bind(this);
-    this.trackUnsubcribed = this.trackUnsubcribed.bind(this);
+    this.trackUnsubscribed = this.trackUnsubscribed.bind(this);
     this.messageReceived = this.messageReceived.bind(this);
     this.roomDisconnected = this.roomDisconnected.bind(this);
     this.dominantSpeakerChanged = this.dominantSpeakerChanged.bind(this);
@@ -85,7 +85,7 @@ export class VideoChat extends EventTarget {
         this.trackSubscribed(participant)(trackPub.track);
       }
       trackPub.on("subscribed", this.trackSubscribed(participant));
-      trackPub.on("unsubscribed", this.trackUnsubcribed(participant));
+      trackPub.on("unsubscribed", this.trackUnsubscribed(participant));
     };
   }
 
@@ -171,7 +171,7 @@ export class VideoChat extends EventTarget {
     };
   }
 
-  trackUnsubcribed(participant) {
+  trackUnsubscribed(participant) {
     return (track) => {
       if (track.kind !== "data") {
         const mediaElements = track.detach();
@@ -188,7 +188,7 @@ export class VideoChat extends EventTarget {
 
   trackUnpublished(trackPub) {
     if (trackPub.track) {
-      this.trackUnscribed(trackPub.track);
+      this.trackUnsubscribed()(trackPub.track);
     }
   }
 
